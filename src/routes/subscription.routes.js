7f8e7257678aaac7fa367e6944fab6cd1b53ec70
Paras-Codes <parassahu06@gmail.com>
2025-09-04 19:55:46 +0530

    webhook route created

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription,createPrimeSubscription } from "../controllers/subscription.controller.js";
-
+import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription,createPrimeSubscription, addPrimeMembership, razorpayWebhookHandler } from "../controllers/subscription.controller.js";
+import { verifyPayments } from "../middlewares/verifypayments.middleware.js";
+import express from "express"
 
 const router=Router()
 
@@ -9,5 +10,9 @@ router.route("/subscribe/:channelId").post(verifyJWT,toggleSubscription)
 router.route("/getsubscribedchannel").get(verifyJWT, getSubscribedChannels)
 router.route("/getsubsciber").get(verifyJWT, getUserChannelSubscribers)
 router.route("/primeSubscription").post(verifyJWT, createPrimeSubscription)
-
+router.route("/verifyPayment").post(verifyJWT,verifyPayments,addPrimeMembership)
+router.route("/razorpay/webhook").post(
+  express.raw({ type: "*/*" }),
+  razorpayWebhookHandler
+);
 export default router;
